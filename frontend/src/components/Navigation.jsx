68561607cfe9c14891840af4cce2dced1caff721
stefanoscotti <stefanogscotti@gmail.com>
2025-08-16 14:53:30 -0400

    updated dots size

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -48,7 +48,7 @@ const Navigation = () => {
             {isAuthenticated ? (
               <div className="relative">
                 <button onClick={() => setIsProfileOpen(!isProfileOpen)} className="text-gray-600 hover:text-blue-600">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={3}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h.01M12 12h.01M19 12h.01" />
                   </svg>
                 </button>
